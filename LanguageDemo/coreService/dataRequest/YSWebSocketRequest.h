53349f98e8a25215678082cb043e240495db0674
bob <bob.yuan@min123.net>
2023-01-04 15:20:39 +0800

    完善webSocket处理逻辑，做了连接失败回调

--- LanguageDemo/coreService/dataRequest/YSWebSocketRequest.h
+++ LanguageDemo/coreService/dataRequest/YSWebSocketRequest.h
@@ -3,13 +3,26 @@
 //  LanguageDemo
 //
 //  Created by Bob on 2023/1/3.
-//
+//  按一条Socket处理
 
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface YSWebSocketRequest : NSObject
+
+
+///  初始化WebSocket
+/// @param server 服务器地址
+/// @param port 端口
+/// @param failureBlock 连接失败回调
+- (id)initWithServer:(NSString *)server port:(NSString *)port connectFailure:(void(^)(void))failureBlock;
+
+/// 连接webSocket
+- (void)refreshConnectWebSocket;
+
+/// 断开webSocket
+- (void)disconnectWebSocket;
 
 @end
 
